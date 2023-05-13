a2130e34e65d7823c5eed89e5cf302cec8bda2b1
jl112 <jasonlee.yn@gmail.com>
2023-05-13 01:33:08 +0000

    functions added

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI
-app= FastAPI()
+# from fastapi import FastAPI
+from flask import Flask, request 
+import pandas as pd
 
-@app.get("/")
-async def root():
-    return 'hello' 
+app = Flask(__name__)
+
+df = pd.read_csv('./data/diagnoses2019.csv')
+
+@app.route('/', methods=["GET"])
+def home():
+    return 'this is the home page'
+
+@app.route('/preview', methods=["GET"])
+def preview():
+    return df.head()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# app = FastAPI()
+# @app.get("/")
+# async def root():
+#     return 'hello' 
